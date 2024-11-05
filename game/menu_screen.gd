1f149b7110dd7fc8a4f23a796a29ed150b7902ea
coltono <coltonormon@shaw.ca>
2024-11-04 23:17:45 -0700

    added settings page with kbm and controller options to be implemented at a later date.

--- game/menu_screen.gd
+++ game/menu_screen.gd
@@ -17,3 +17,8 @@ func _on_level_browser_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://settings.tscn")
