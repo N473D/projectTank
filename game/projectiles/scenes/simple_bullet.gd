6d84aec485a6a60208662fbb4f8555ae2fdca2b7
N DeBliek <ndebl589@mtroyal.ca>
2024-11-23 20:34:57 -0700

    Pushing inital projectile setup, need to test for collitions and bounces

---
+++ game/projectiles/scenes/simple_bullet.gd
@@ -0,0 +1,5 @@
+extends RigidBody2D
+class_name SimpleBullet
+
+func _ready() -> void:
+	gravity_scale = 0.0
--- game/tanks/scenes/parts/turrets/default_turret_template.gd
+++ game/tanks/scenes/parts/turrets/default_turret_template.gd
@@ -3,8 +3,17 @@ extends Node2D
 class_name Turret
 
 @export var rotation_speed : float = 0.5
+@export var barrelCollection : Node2D
+var numBarrel  : int = 0
+
 var roateTo : float = 0.0
 
+var loaded : bool = true
+var readyToFire : bool = true
+@export var roundsChambered : int = 1
+@export var maxRounds : int = 1
+@export var reloadTime : Timer
+@export var fire_rate : Timer
 
 func _on_controller_component_target(targeting : Vector2) -> void:
 	#print("Vector: ", targeting)
@@ -22,5 +31,24 @@ func _process(_delta: float) -> void:
 	#print("Target: ", roateTo)
 	#print("After: ", rotation)
 
-func fire_bullet(_delta: float) -> bool:
-	return false
+func canFire() -> bool:
+	return (loaded and readyToFire)
+
+
+func _on_tank_spawn_bullet(shell, speed:int) -> void:
+	barrelCollection.get_child((numBarrel % barrelCollection.get_child_count())).spawnRidged(shell.instantiate(), Vector2(speed, 0).rotated(rotation - (PI/2)), rotation)
+	numBarrel += 1
+	roundsChambered -= 1
+	if roundsChambered <= 0:
+		loaded = false
+		reloadTime.start()
+	roundsChambered = false
+	fire_rate.start()
+
+
+func _on_shot_delay_timeout() -> void:
+	loaded = true
+
+
+func _on_reload_delay_timeout() -> void:
+	roundsChambered = true
