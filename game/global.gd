242d45019f8653127969e941439bb3cb078f0916
coltono <coltonormon@shaw.ca>
2024-12-02 19:09:15 -0700

    Working pause menu added.

--- game/global.gd
+++ game/global.gd
@@ -3,12 +3,19 @@ extends Node
 var previousScene = ""
 var defaultTankScene = load("res://tanks/scenes/parts/tank.tscn")
 var playerTanks = [] #when updated, call updatePlayers function that will spawn in players.
+var paused = false
 
 func updatePlayers() -> void:
 	pass
 
 func addPlayer() -> void:
 	playerTanks.append(defaultTankScene.instantiate())
+
+func despawn_tanks() -> void:
+	for tank in Global.playerTanks:
+		if tank.get_parent():
+			tank.get_parent().remove_child(tank)
+
 
 func get_previous_screne() -> String:
 	return previousScene
@@ -23,4 +30,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("pause"):
+		if get_tree().current_scene.get_node_or_null("PauseMenu"):
+			get_tree().current_scene.get_node("PauseMenu").toggle()
+
+	
+		
---
+++ game/menus/pause_menu.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_quit_pressed() -> void:
+	Global.despawn_tanks()
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://menus/menu_screen.tscn")
+
+func _on_resume_pressed() -> void:
+	toggle()
+	
+func toggle() -> void:
+	if !self.visible:
+		self.visible = true
+		get_tree().paused = true
+	else:
+		self.visible = false
+		get_tree().paused = false
